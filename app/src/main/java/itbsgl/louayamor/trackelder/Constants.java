7475d3c5c72b7078766b61a5210d2553ca8261ea
louayamor <amor.louay20@gmail.com>
2025-02-05 11:32:00 +0100

    Added request sms permission

---
+++ app/src/main/java/itbsgl/louayamor/trackelder/Constants.java
@@ -0,0 +1,6 @@
+package itbsgl.louayamor.trackelder;
+
+public class Constants {
+
+    public static final String MSG_SENDME_POSITION ="Send your position";
+}
--- app/src/main/java/itbsgl/louayamor/trackelder/ui/home/HomeFragment.java
+++ app/src/main/java/itbsgl/louayamor/trackelder/ui/home/HomeFragment.java
@@ -1,6 +1,7 @@
 package itbsgl.louayamor.trackelder.ui.home;
 
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,20 +13,33 @@ import androidx.lifecycle.ViewModelProvider;
 
 import itbsgl.louayamor.trackelder.databinding.FragmentHomeBinding;
 
+import itbsgl.louayamor.trackelder.Constants;
+
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        binding.btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String number = binding.editTextPhone.getText().toString();
+                SmsManager manager = SmsManager.getDefault();
+                manager.sendTextMessage(
+                        number,
+                        null,
+                        Constants.MSG_SENDME_POSITION,
+                        null,
+                        null
+                        );
+            }
+        } );
         return root;
     }
 
